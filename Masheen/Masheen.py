c2fc9e3327042fe51ad55fe541b0d70a9991e23c
michael <micha2718l@gmail.com>
2019-11-29 15:34:11 -0600

    debuggering, proper bracket stuff, wrapping data

--- Masheen/Masheen.py
+++ Masheen/Masheen.py
@@ -1,20 +1,27 @@
 class Masheen():
     validCommands = '+-><[].,'
     
-    def __init__(self, prog):
-        self.dataProg = [0]*10
+    def __init__(self, prog, dataIn=[0], debuggering=False):
+        self.maxValue = 256
+        self.dataIn = dataIn
+        self.pointIn = 0
+        self.dataN = 10
+        self.dataProg = [0]*self.dataN
         self.dataCode = [di for di in prog if di in self.validCommands]
         self.maxStep = 1000
         self.pointData = 0
         self.pointCode = 0
         self.pointJumps = []
         self.outs = []
-        #print(self.dataCode)
+        self.debuggering = debuggering
+        if self.debuggering:
+            print(self.dataCode)
     def run(self):
         if len(self.dataCode)==0:
             return
         for i in range(self.maxStep):
-            #print(f'iter {i}, code {self.dataCode[self.pointCode]}')
+            if self.debuggering:
+                print(f'iter {i}, code {self.dataCode[self.pointCode]}')
             if self.dataCode[self.pointCode] == '+':
                 self.dataProg[self.pointData] += 1
             elif self.dataCode[self.pointCode] == '-':
@@ -24,7 +31,25 @@ class Masheen():
             elif self.dataCode[self.pointCode] == '<':
                 self.pointData -= 1
             elif self.dataCode[self.pointCode] == '[':
-                self.pointJumps.append(self.pointCode)
+                if self.dataProg[self.pointData] == 0:
+                    #print('?')
+                    deep = 0
+                    newpointCode = None
+                    for di, d in enumerate(self.dataCode[self.pointCode+1:]):
+                        #print(d)
+                        #print(deep)
+                        #print(di)
+                        if d == ']' and deep == 0:
+                            newpointCode = di + self.pointCode + 1
+                        elif d == ']' and deep > 0:
+                            deep -= 1
+                        if d == '[':
+                            deep += 1
+                    if newpointCode is None:
+                        raise ValueError("Can't find proper matching ']'.")
+                    self.pointCode = newpointCode
+                else:
+                    self.pointJumps.append(self.pointCode)
             elif self.dataCode[self.pointCode] == ']':
                 if len(self.pointJumps) == 0:
                     self.pointJumps.append(0)
@@ -34,12 +59,26 @@ class Masheen():
                     del self.pointJumps[-1]
             elif self.dataCode[self.pointCode] == '.':
                 self.outs.append(self.dataProg[self.pointData])
-                
+            elif self.dataCode[self.pointCode] == ',':
+                self.dataProg[self.pointData] = self.dataIn[self.pointIn]
+                self.pointIn += 1
+                self.pointIn = self.pointIn % len(self.dataIn)
+            
             self.pointCode +=1
-            #print(self.dataProg)
-            #print(f'pointData {self.pointData} \npointCode {self.pointCode}')
+            self.pointData = self.pointData % self.dataN
+            self.dataProg = [d % self.maxValue for d in self.dataProg]
+            if self.debuggering:
+                print(''.join([f'*{d}*' if di==self.pointData else f' {d} ' for di, d in enumerate(self.dataProg)]))
+            if self.debuggering:
+                print(self.dataProg)
+                print(f'pointData {self.pointData} \npointCode {self.pointCode}')
             if self.pointCode >= len(self.dataCode):
                 break
+
+def Mash(prog):
+    m = Masheen(prog)
+    m.run()
+    return ''.join([chr(mi) for mi in m.outs])
 
 if __name__ == '__main__':            
     c = '+>++<-'
