265f4618cf1106183debdf79de200204f3513fc1
Michael Haas <micha2718l@gmail.com>
2019-12-04 12:20:16 -0600

    version bump, twine is annoying

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Masheen',
-      version='0.3',
+      version='0.31',
       description='A Masheen for brainfuck.',
       url='http://github.com/micha2718l/Masheen',
       author='micha2718l',
